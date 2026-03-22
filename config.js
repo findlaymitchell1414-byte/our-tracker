d031ad3679ca4da843aa3ff9287cd9a67fbc96c8
findlaymitchell1414-byte <findlaymitchell1414@gmail.com>
2026-03-22 20:15:26 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,21 @@
 
-
 const CONFIG = {
-
+ 
   // 🔧 Shared password to access the app
   password: 'finnybean!',
-
+ 
   // 🔧 Your names as they appear in the app
   names: {
-    p1: 'Fin',   // e.g. 'Alex'
-    p2: 'Em'    // e.g. 'Jordan'
+    p1: 'Em',   // e.g. 'Alex'
+    p2: 'Fin'    // e.g. 'Jordan'
   },
-
-  // 🔧 Daily calorie goal per person (kcal)
-  calorieGoal: 2000,
-
+ 
+  // 🔧 Daily calorie goals per person (kcal)
+  calorieGoals: {
+    p1: 1668,  // 🔧 e.g. 2000
+    p2: 2100   // 🔧 e.g. 1800
+  },
+ 
   // 🔧 Firebase config — from your Firebase project settings
   firebase: {
     apiKey: "AIzaSyDH_7YYgamMLRJv2xZxX8uO47BJUhlC0P0",
@@ -24,5 +26,5 @@ const CONFIG = {
     messagingSenderId: "1098550772858",
     appId: "1:1098550772858:web:3926088219c59a4d12e7c3"
   }
-
+ 
 };
